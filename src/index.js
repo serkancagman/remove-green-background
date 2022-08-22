6770bea74a3ff03cd410a5fbe2f6a11dfa29f3d1
serkancagman <serkancagmann@gmail.com>
2022-08-22 16:28:46 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,55 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = ({
+  elementRef,
+  rColor,
+  gColor,
+  bColor,
+  width,
+  height
+}) => {
+  const canvasRef = React.createRef(null)
+
+  React.useEffect(() => {
+    if (canvasRef.current && elementRef.current && elementRef) {
+      const interval = setInterval(() => {
+        const ctx = canvasRef?.current?.getContext('2d')
+        ctx?.drawImage(
+          elementRef.current,
+          0,
+          0,
+          canvasRef.current.width,
+          canvasRef.current.height
+        )
+        let frame =
+          ctx &&
+          ctx.getImageData(
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+          )
+        let data = frame && frame.data.length / 4
+        for (let i = 0; i < data; i++) {
+          let r = frame.data[i * 4 + 0]
+          let g = frame.data[i * 4 + 1]
+          let b = frame.data[i * 4 + 2]
+          if (g > gColor && r < rColor) frame.data[i * 4 + 3] = 0
+        }
+        ctx && ctx.putImageData(frame, 0, 0)
+      }, 10)
+      return () => clearInterval(interval)
+    }
+  })
+
+  return (
+    <canvas
+      onMouseOver={() => elementRef.current && elementRef.current.pause()}
+      onMouseOut={() => elementRef.current && elementRef.current.play()}
+      ref={canvasRef}
+      width={width}
+      height={height}
+    />
+  )
 }
